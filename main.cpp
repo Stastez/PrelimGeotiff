e92fc4028eb28f3f0a1dd25fc3e9ec14262f21a2
Sandro Steeger <sandro.steeger.stuff@gmail.com>
2023-05-07 19:11:58 +0200

    Reconfigured for GDAL

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
-#include "libgeotiff.h"
+#include <gdal_priv.h>
 
 int main() {
-
+    GDALDataset* dataset = (GDALDataset*) GDALOpen("cmake-build-debug/data/byte.tif", GA_ReadOnly);
+    dataset.
 
     return 0;
 }
